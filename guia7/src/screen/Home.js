96bce99b0b72051d3f55e7e98db678f076bef77c
Andrea Elías <54760483+Andrea-EH@users.noreply.github.com>
2021-10-24 21:10:55 -0600

    Add files via upload

---
+++ guia7/src/screen/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'; import {View,Text,Button} from 'react-native'; export default function Home(props)
+const{navigation}= props;(
+
+{ 
+  return(
+     <View>
+      <Text> Estamos en Home </Text>
+       </View> ); 
+       }
+);
